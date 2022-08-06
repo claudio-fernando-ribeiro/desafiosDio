53c80d34270cc50fc7fdfc1f493a9a0cb8906998
claudio-fernando-ribeiro <claudiofernando1989@gmail.com>
2022-08-06 19:18:46 -0300

    desafios

---
+++ desadio1.ts
@@ -0,0 +1,6 @@
+
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {Number, String};
+employee.code = 10;
+employee.name = "John";
---
+++ desafio2.ts
@@ -0,0 +1,29 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {nomeasync (params:type): any => {
+    nome String, (idade: Number), profissao: String};	
+} 
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}
+
+function nome(nome: any, String: StringConstructor) {
+    throw new Error("Function not implemented.");
+}
